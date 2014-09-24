48d7fb92bac94b896b15dac7f1edf629fe6dce1f
Dominic Böttger <Dominic.Boettger@inspirationlabs.com>
2014-09-24 04:47:37 +0200

    ipv6
    Signed-off-by: Dominic Böttger <Dominic.Boettger@inspirationlabs.com>

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ var app = express();
 var oneDay = 86400000;
 
 app.use(express.static(__dirname + '/bower_components/mlcl_admin', { maxAge: oneDay }));
-var server = app.listen(process.env.PORT || 3000, function(err) {
+var server = app.listen(process.env.PORT || 3000, '::', function(err) {
   console.log('MOLECUEL admin listening on port %d', server.address().port);
 });
