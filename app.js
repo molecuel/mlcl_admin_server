c86d94b7405625138ab9ec3cb7dae1fb5f7a3079
Dominic Böttger <Dominic.Boettger@inspirationlabs.com>
2014-09-24 04:20:13 +0200

    Initial commit
    Signed-off-by: Dominic Böttger <Dominic.Boettger@inspirationlabs.com>

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+var oneDay = 86400000;
+
+app.use(express.static(__dirname + '/bower_components/mlcl_admin', { maxAge: oneDay }));
+var server = app.listen(process.env.PORT || 3000, function(err) {
+  console.log('MOLECUEL admin listening on port %d', server.address().port);
+});
